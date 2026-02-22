1f8dcc8ddeab735be5047c19baba559fa61cf040
nginxdev <q4pradeep@gmail.com>
2026-02-22 18:45:27 +0100

    style: remove features section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Hero } from './components/Hero';
-import { Features } from './components/Features';
 
 function App() {
     return (
@@ -14,7 +13,6 @@ function App() {
             <div className="relative z-10 flex flex-col min-h-screen justify-between">
                 <div>
                     <Hero />
-                    <Features />
                 </div>
 
                 <footer className="py-8 text-center text-neutral-500 text-sm border-t border-neutral-200 bg-white/50 backdrop-blur-md">
